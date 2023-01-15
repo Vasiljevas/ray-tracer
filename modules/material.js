e6058a91d89257aff6b117adba05dbc0ab495d25
vasiljevas <andrius.vasiljevas@nortal.com>
2023-01-15 07:28:57 +0200

    final uddate

--- modules/material.js
+++
@@ -1,3 +0,0 @@
-export class Material {
-    getColorAt = () => { throw("Classes which extend Material must implement getColorAt"); }
-}
--- modules/settings.js
+++ modules/settings.js
@@ -1,3 +1,3 @@
 export const THRESHOLD = 0.000001;
 export const MAX_DEPTH = 8;
-export const MAX_SHAPES_PER_NODE = 51;
+export const MAX_SHAPES_PER_NODE = 8;
